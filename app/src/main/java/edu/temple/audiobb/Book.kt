8cd46c8183afd071a6e37797028e3250d5c4f2cb
CoFlo35 <43580764+CoFlo35@users.noreply.github.com>
2021-11-15 14:45:25 -0500

    Initial Commit - added duration value to Book class, added and implemented a dependency for the PlayerService

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -2,6 +2,6 @@ package edu.temple.audiobb
 
 import java.io.Serializable
 
-data class Book (val title: String, val author:String, val id:Int, val coverURL:String):Serializable{
+data class Book (val title: String, val author:String, val id:Int, val coverURL:String, val duration:Int):Serializable{
 
 }
